ecb6a9f8a2697087f6cf4ae4324ddef6120253d6
armstrongmsg <armstrongmsg@gmail.com>
2015-07-18 14:10:33 -0300

    Complementei a checagem de argumentos na detecção de faces.

--- FaceDetection/src/main.cpp
+++ FaceDetection/src/main.cpp
@@ -16,14 +16,10 @@
 using namespace std;
 using namespace cv;
 
-int face_detection_demo();
+void face_detection(string input_image, string output_image);
 
-int face_detection_demo()
+void face_detection(string input_image, string output_image)
 {
-    Mat image;
-	char input_image[100];
-	char output_image[100];
-
 	// Load Face cascade (.xml file)
 	Detector detector("conf/haarcascade_frontalface_alt2.xml",
 			"conf/haarcascade_righteye_2splits.xml",
@@ -32,57 +28,32 @@ int face_detection_demo()
 			"conf/haarcascade_eye.xml");
 
 	std::vector<Rect> faces;
-	std::vector<Rect> right_eyes;
-	std::vector<Rect> left_eyes;
-	std::vector<Rect> smiles;
-	std::vector<Rect> eyes;
 
-	for (int j = 1; j <= 11; j++)
+	Mat image = imread(input_image, CV_LOAD_IMAGE_COLOR);
+
+	// Detect faces
+	faces = detector.detect_faces(image);
+
+	if (faces.size() > 1)
 	{
-		sprintf(input_image, "images/%d.jpg", j);
-		sprintf(output_image, "result/%d.jpg", j);
-		image = imread(input_image, CV_LOAD_IMAGE_COLOR);
-
-		// Detect faces
-		faces = detector.detect_faces(image);
-		right_eyes = detector.detect_right_eye(image);
-		left_eyes = detector.detect_left_eye(image);
-		smiles = detector.detect_smile(image);
-		eyes = detector.detect_eyes(image);
-		// Draw circles on the detected faces
-		for( int i = 0; i < faces.size(); i++ )
-		{
-			rectangle(image, faces[i], Scalar(0, 255, 0));
-		}
-
-		/*for( int i = 0; i < right_eyes.size(); i++ )
-		{
-			rectangle(image, right_eyes[i], Scalar(255, 0, 0));
-		}
-
-		for( int i = 0; i < left_eyes.size(); i++ )
-		{
-			rectangle(image, left_eyes[i], Scalar(0, 0, 255));
-		}*/
-
-		for( int i = 0; i < smiles.size(); i++ )
-		{
-			rectangle(image, smiles[i], Scalar(255, 0, 255));
-		}
-
-		for( int i = 0; i < eyes.size(); i++ )
-		{
-			rectangle(image, eyes[i], Scalar(255, 0, 0));
-		}
-
-		imwrite(output_image, image);
+		puts("WARNING: Achou mais de uma face!");
 	}
 
-	return 0;
+	imwrite(output_image, image(faces[0]));
 }
 
-int main( int argc, char** argv )
+void check_arguments(int argc, char** argv)
 {
-	face_detection_demo();
+	if (argc != 3)
+	{
+		puts("Numero incorreto de argumentos");
+		exit(-1);
+	}
+}
+
+int main(int argc, char** argv)
+{
+	check_arguments(argc, argv);
+	face_detection(argv[1], argv[2]);
 }
 
